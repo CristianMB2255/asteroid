476567ae920189c6c75b39cdf519cfea50c2d6b8
CristianMB2255 <84812535+CristianMB2255@users.noreply.github.com>
2023-07-20 12:48:10 -0300

    Update asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -85,7 +85,7 @@ async def _id_check(session, item_id):
             for i in range(4):
                 await buy_item(session, data)
 
-        elif data.get('Remaining') == 0:
+        elif item_stats.get('Remaining') == 0:
             config['item'].remove(item_id)
             with open("config.json", "w") as file:
                 json.dump(config, file, indent=1)
